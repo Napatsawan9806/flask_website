25638d38ca4abcf46292e2530d281f58ca3a6f80
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-02-14 18:13:52 +0700

    try login btn

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,8 +8,22 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/success/<name>")
+def success(name):
+    return "Welcome %s" % name
+
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    print(request.method)
+    if request.method == "POST":
+        if request.form.get("login") == "Login":
+            print("login")
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html")
+    else:
+        pass
     return render_template("login.html")
 
 
