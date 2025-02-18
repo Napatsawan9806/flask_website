7dc5ddedcdd5e120b857c361342204b1ce227294
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-02-18 13:09:19 +0700

    add more login manager

--- project/app.py
+++ project/app.py
@@ -24,6 +24,11 @@ login_manager.init_app(app)
 login_manager.login_view = "Login"
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(int(user_id))
+
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     print(request.method)
