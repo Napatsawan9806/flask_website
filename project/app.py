8cf3d435c99febeef867c3b7042860c5f17cb515
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-02-18 12:39:32 +0700

    add database

--- project/app.py
+++ project/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
 
 @app.route("/", methods=["POST", "GET"])
