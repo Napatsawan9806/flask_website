85935e7f6d62f57a77de11a2f170fb3cdf4c7d04
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-02-21 12:40:43 +0700

    add more

--- project/models.py
+++ project/models.py
@@ -17,3 +17,17 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    schedule = db.Column(db.String(50), nullable=False)
+
+
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
+    user = db.relationship("User", backref="enrollments")
+    course = db.relationship("Course", backref="enrolled_students")
